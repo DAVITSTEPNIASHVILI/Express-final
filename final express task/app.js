00fba71ad352f810076103ed1079ecc9d280c860
DAVITSTEPNIASHVILI <davitstepniashvili4@gmail.com>
2026-04-14 19:04:11 +0400

    commit

--- final express task/app.js
+++ final express task/app.js
@@ -1,7 +1,6 @@
 import express from "express"
 import dotenv from "dotenv"
 import booksRouter from "./Routers/books.route.js"
-import morgan from "morgan"
 
 const app = express()
 
@@ -10,7 +9,5 @@ dotenv.config()
 app.use(express.json())
 
 app.use("/books", booksRouter)
-
-app.use(morgan("dev"))
 
 app.listen(process.env.PORT, () => console.log(`Server is running on port ${process.env.PORT}`))
